c32e305e4fb847b6ef95af6225499195953ae5fe
ColetonS <coleton.m.smith@gmail.com>
2019-10-07 12:37:14 -0600

    finished 5

--- server.js
+++ server.js
@@ -7,5 +7,9 @@ const app = express()
 app.use(express.json())
 
 app.get('/api/user', usersCtrl.getUsers)
+app.get('/api/user/:userId', usersCtrl.getUserById)
+app.get('/api/admin', usersCtrl.getAdmins)
+app.get('/api/nonadmin', usersCtrl.getNonAdmins)
+app.get('/api/type/:userType', usersCtrl.getUserByType)
 
 app.listen(SERVER_PORT, () => console.log(`Listening on port ${SERVER_PORT}`))
--- usersCtrl.js
+++ usersCtrl.js
@@ -8,19 +8,45 @@ module.exports = {
       });
       res.status(200).send(filteredUsers);
     } else if (req.query.email) {
-        const filteredUsers = data.filter(user => {
-            return user.email === req.query.email
-        })
-        res.status(200).send(filteredUsers)
-    }
-     else if (req.query.favorites) {
-         const filteredUsers = data.filter(user => {
-             return user.favorites.includes(req.query.favorites)
-         })
-         res.status(200).send(filteredUsers)
-     }
-    else {
+      const filteredUsers = data.filter(user => {
+        return user.email === req.query.email;
+      });
+      res.status(200).send(filteredUsers);
+    } else if (req.query.favorites) {
+      const filteredUsers = data.filter(user => {
+        return user.favorites.includes(req.query.favorites);
+      });
+      res.status(200).send(filteredUsers);
+    } else {
       res.status(200).send(data);
     }
+  },
+  getUserById(req, res) {
+    const user = data.filter(user => {
+      return user.id === +req.params.userId;
+    });
+    if (user !== []) {
+        res.status(200).send(user)
+    } else {
+        res.status(404).send('User does not exist')
+    }
+  },
+  getAdmins(req, res) {
+      const admins = data.filter(user => {
+          return user.type === 'admin'
+      })
+      res.status(200).send(admins)
+  },
+  getNonAdmins(req, res) {
+      const nonAdmins = data.filter(user => {
+          return user.type === 'user'
+      })
+      res.status(200).send(nonAdmins)
+  },
+  getUserByType(req, res) {
+      const filteredUsers = data.filter(user => {
+          return user.type === req.params.userType
+      })
+      res.status(200).send(filteredUsers)
   }
 };
